cb73197140b9dd8388d8da3d4a7019d04f655e16
Forever Gone <78507032+Faint0002@users.noreply.github.com>
2023-02-25 18:11:17 -0500

    Update movement.cpp

--- DXBasic/src/script/self/movement/movement.cpp
+++ DXBasic/src/script/self/movement/movement.cpp
@@ -1,10 +1,9 @@
 #include "script/self/movement/movement.h"
 
 namespace script {
-	auto s_m_cfg = g_config.get()["self"]["movement"];
 	movement::movement() : submenu("Movement", &movement::call) {}
 	void movement::call(submenu& sub) {
-		sub.add(booleanOption("Super Run", s_m_cfg["superRun"].get<bool*>()));
-		sub.add(booleanOption("Super Jump", s_m_cfg["superJump"].get<bool*>()));
+		sub.add(booleanOption("Super Run", &features::self::movement::superRunBool));
+		sub.add(booleanOption("Super Jump", &features::self::movement::superJumpBool));
 	}
 }
